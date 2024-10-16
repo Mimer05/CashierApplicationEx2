41348e49e810097481e55d9a54f33f7083a22968
Misyel E <Misyel E@MISYEL>
2024-10-16 22:52:33 +0800

    Add project files.

---
+++ CashierApplication/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace CashierApplication
+{
+    partial class frmPurchaseDiscountedItem
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            TBItem = new TextBox();
+            label3 = new Label();
+            TBPrice = new TextBox();
+            TBDiscount = new TextBox();
+            TBQuantity = new TextBox();
+            label4 = new Label();
+            BCompute = new Button();
+            label5 = new Label();
+            LBAmount = new Label();
+            label7 = new Label();
+            TBPayment = new TextBox();
+            BSubmit = new Button();
+            label8 = new Label();
+            LBChange = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Bahnschrift Condensed", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(88, 35);
+            label1.Name = "label1";
+            label1.Size = new Size(47, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Item:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Bahnschrift Condensed", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(479, 35);
+            label2.Name = "label2";
+            label2.Size = new Size(99, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Discount(%):";
+            // 
+            // TBItem
+            // 
+            TBItem.Location = new Point(88, 75);
+            TBItem.Name = "TBItem";
+            TBItem.Size = new Size(240, 23);
+            TBItem.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Bahnschrift Condensed", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(88, 124);
+            label3.Name = "label3";
+            label3.Size = new Size(51, 25);
+            label3.TabIndex = 3;
+            label3.Text = "Price:";
+            // 
+            // TBPrice
+            // 
+            TBPrice.Location = new Point(145, 126);
+            TBPrice.Name = "TBPrice";
+            TBPrice.Size = new Size(183, 23);
+            TBPrice.TabIndex = 4;
+            // 
+            // TBDiscount
+            // 
+            TBDiscount.Location = new Point(479, 75);
+            TBDiscount.Name = "TBDiscount";
+            TBDiscount.Size = new Size(128, 23);
+            TBDiscount.TabIndex = 5;
+            // 
+            // TBQuantity
+            // 
+            TBQuantity.Location = new Point(502, 130);
+            TBQuantity.Name = "TBQuantity";
+            TBQuantity.Size = new Size(105, 23);
+            TBQuantity.TabIndex = 6;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Bahnschrift Condensed", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(420, 124);
+            label4.Name = "label4";
+            label4.Size = new Size(76, 25);
+            label4.TabIndex = 7;
+            label4.Text = "Quantity:";
+            // 
+            // BCompute
+            // 
+            BCompute.BackColor = SystemColors.ActiveCaption;
+            BCompute.BackgroundImageLayout = ImageLayout.Center;
+            BCompute.FlatStyle = FlatStyle.Popup;
+            BCompute.Font = new Font("Arial Narrow", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            BCompute.ForeColor = SystemColors.ButtonHighlight;
+            BCompute.Location = new Point(341, 176);
+            BCompute.Name = "BCompute";
+            BCompute.Size = new Size(104, 29);
+            BCompute.TabIndex = 8;
+            BCompute.Text = "Compute";
+            BCompute.UseVisualStyleBackColor = false;
+            BCompute.Click += BCompute_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Bahnschrift Condensed", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(220, 239);
+            label5.Name = "label5";
+            label5.Size = new Size(108, 25);
+            label5.TabIndex = 9;
+            label5.Text = "Total amount:";
+            // 
+            // LBAmount
+            // 
+            LBAmount.AutoSize = true;
+            LBAmount.Font = new Font("Bahnschrift Condensed", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            LBAmount.Location = new Point(351, 239);
+            LBAmount.Name = "LBAmount";
+            LBAmount.Size = new Size(94, 25);
+            LBAmount.TabIndex = 10;
+            LBAmount.Text = "The amount";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Bahnschrift Condensed", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.Location = new Point(159, 298);
+            label7.Name = "label7";
+            label7.Size = new Size(140, 25);
+            label7.TabIndex = 11;
+            label7.Text = "Payment recieved:";
+            // 
+            // TBPayment
+            // 
+            TBPayment.Location = new Point(314, 304);
+            TBPayment.Name = "TBPayment";
+            TBPayment.Size = new Size(150, 23);
+            TBPayment.TabIndex = 12;
+            // 
+            // BSubmit
+            // 
+            BSubmit.BackColor = SystemColors.ActiveCaption;
+            BSubmit.BackgroundImageLayout = ImageLayout.Center;
+            BSubmit.FlatStyle = FlatStyle.Popup;
+            BSubmit.Font = new Font("Arial Narrow", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            BSubmit.ForeColor = SystemColors.ButtonFace;
+            BSubmit.Location = new Point(503, 304);
+            BSubmit.Name = "BSubmit";
+            BSubmit.Size = new Size(104, 33);
+            BSubmit.TabIndex = 13;
+            BSubmit.Text = "Submit";
+            BSubmit.UseVisualStyleBackColor = false;
+            BSubmit.Click += BSubmit_Click;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Bahnschrift Condensed", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label8.Location = new Point(260, 366);
+            label8.Name = "label8";
+            label8.Size = new Size(68, 25);
+            label8.TabIndex = 14;
+            label8.Text = "Change:";
+            // 
+            // LBChange
+            // 
+            LBChange.AutoSize = true;
+            LBChange.Font = new Font("Bahnschrift Condensed", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            LBChange.Location = new Point(351, 366);
+            LBChange.Name = "LBChange";
+            LBChange.Size = new Size(91, 25);
+            LBChange.TabIndex = 15;
+            LBChange.Text = "The change";
+            // 
+            // frmPurchaseDiscountedItem
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Lavender;
+            BackgroundImageLayout = ImageLayout.Center;
+            ClientSize = new Size(800, 450);
+            Controls.Add(LBChange);
+            Controls.Add(label8);
+            Controls.Add(BSubmit);
+            Controls.Add(TBPayment);
+            Controls.Add(label7);
+            Controls.Add(LBAmount);
+            Controls.Add(label5);
+            Controls.Add(BCompute);
+            Controls.Add(label4);
+            Controls.Add(TBQuantity);
+            Controls.Add(TBDiscount);
+            Controls.Add(TBPrice);
+            Controls.Add(label3);
+            Controls.Add(TBItem);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            DoubleBuffered = true;
+            Name = "frmPurchaseDiscountedItem";
+            Text = "Purchase Discounted Item";
+            Load += frmPurchaseDiscountedItem_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox TBItem;
+        private Label label3;
+        private TextBox TBPrice;
+        private TextBox TBDiscount;
+        private TextBox TBQuantity;
+        private Label label4;
+        private Button BCompute;
+        private Label label5;
+        private Label LBAmount;
+        private Label label7;
+        private TextBox TBPayment;
+        private Button BSubmit;
+        private Label label8;
+        private Label LBChange;
+    }
+}
---
+++ CashierApplication/Form1.cs
@@ -0,0 +1,117 @@
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using CashierApplication;
+using ItemNamespace;
+
+namespace CashierApplication
+{
+    public partial class frmPurchaseDiscountedItem : Form
+    {
+        private DiscountedItem item; //declaring object of the other classes in other namespace
+        public frmPurchaseDiscountedItem()
+        {
+            InitializeComponent();
+        }
+        private void frmPurchaseDiscountedItem_Load(object sender, EventArgs e)
+        {
+        }
+        private void BCompute_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // To know if the text box is empty 
+                if (string.IsNullOrWhiteSpace(TBItem.Text) ||
+                    string.IsNullOrWhiteSpace(TBPrice.Text) ||
+                    string.IsNullOrWhiteSpace(TBQuantity.Text) ||
+                    string.IsNullOrWhiteSpace(TBDiscount.Text))
+                {
+                    MessageBox.Show("Please input the item name, price, discount, and quantity.");
+                    return;
+                }
+                // the inputs of user
+                string ItemName = TBItem.Text;
+                double ItemPrice = Convert.ToDouble(TBPrice.Text);
+                int ItemQuantity = Convert.ToInt32(TBQuantity.Text);
+                double Discount = Convert.ToDouble(TBDiscount.Text);
+
+                item = new DiscountedItem(ItemName, ItemPrice, ItemQuantity, Discount); // instance of DiscountedItem and then get the input of user
+
+                double TotalAmount = item.getTotalPrice();  // Calculate total amount and display it in label
+                LBAmount.Text = TotalAmount.ToString("F2");
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Please enter numbers for price, discount, and quantity."); //when the input of user is not a number to calculate then show this
+            }
+        }
+        private void BSubmit_Click(object sender, EventArgs e)
+        {
+            if (item == null) // when the item is no input and payment then you cant change
+            {
+                MessageBox.Show("Compute first the item.");
+                return;
+            }
+            try
+            {
+                double Payment = Convert.ToDouble(TBPayment.Text); // input of user in payment 
+
+                item.setPayment(Payment); // Set the payment to calculate the change
+                double change = item.getChange();
+
+                LBChange.Text = change.ToString("F2"); // Display the change
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Please enter a number for payment."); //when the payment is not number 
+            }
+        }
+    }
+}
+    namespace ItemNamespace // new namespace for the 2 class
+    {
+        public abstract class Item // making it abstract class for security purposes and efficient
+        {  //declaring the needed variable
+            protected string item_name;
+            protected double item_price;
+            protected int item_quantity;
+            private double total_price;
+
+            public Item(string name, double price, int quantity) // the constructor
+            {
+                this.item_name = name;
+                this.item_price = price;
+                this.item_quantity = quantity;
+            }
+            public abstract double getTotalPrice(); // abstract method
+            public abstract void setPayment(double amount); //this one too
+
+        }
+    
+        public class DiscountedItem : Item //discountedItem the subclass of baseclass Item
+        {// the needed variable and declaring it to show the access modiffier 
+            private double item_discount;
+            private double discounted_price;
+            private double payment_amount;
+            private double change;
+
+            public DiscountedItem(string name, double price, int quantity, double discount) : base(name, price, quantity) // the constructor
+            {
+                this.item_discount = discount;
+            }
+            public override double getTotalPrice() // overriding the abstract method of gettotalprice
+            {
+                double total = item_price * item_quantity;
+                double discountAmount = total * (this.item_discount * 0.01);
+                return total - discountAmount;
+            }
+            public override void setPayment(double amount) // overriding the abstract method setpayment
+            {
+                this.payment_amount = amount;
+            }
+            public double getChange() //the method that calculate the change
+            {
+                double total = getTotalPrice();
+                return this.payment_amount - total;
+            }
+        }
+    }
